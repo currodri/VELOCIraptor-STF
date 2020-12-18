da5e2a47b3eaa61368950c46d92558b6f5a5bf3b
JinsuRhee <jinsu.rhee@gmail.com>
2020-12-18 07:08:49 +0000

    Perform test ver

--- src/js_perform.h
+++ src/js_perform.h
@@ -1,5 +1,5 @@
-#define JS_ADT_ON
+//#define JS_ADT_ON
 #define JS_NODECLOSE_ON
-#define JS_NODESKIP_S_ON
-#define JS_NODESKIP_L_ON
-#define JS_NEWSKIP_ON
+#define JS_NODESKIP_ON
+
+#define JS_LEAFTIME_ON
